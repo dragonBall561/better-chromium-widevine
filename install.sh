97419bf7f7811277a3802cee2d1114c944a2f37f
dragonBall561 <61358474+dragonBall561@users.noreply.github.com>
2020-10-09 12:41:20 -0400

    Added Install Stuff

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# Making You Stop Chromium
+read -p 'Please Exit Chromium Now, then click enter' a
+export installdir=$(pwd)/Files
+
+# Telling You What to do :)
+echo Getting WideVine Supporting Chromium
+echo Please Allow the Stuff
+echo I\'m going to be 'apt update'-ing a lot
+
+# Adding Widevine Repo
+sudo add-apt-repository ppa:saiarcot895/chromium-beta
+sudo apt update
+
+# Removing Snap
+sudo snap remove chromium
+
+# Removing old Chromium
+sudo apt purge chromium-browser chromium chromium-codecs-ffmpeg-extra chromium-browser-l10n chromium-codecs-ffmpeg
+
+# Installing New Chromium
+sudo apt install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg-extra
+
+# Getting Chrome Deb
+mkdir chrome && cd chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Extracting Chrome Deb
+mkdir c
+dpkg -x ./*.deb ./c
+cd c
+cd opt/google/chrome/
+
+# Moving Widevine
+sudo cp -r WidevineCdm /usr/lib/chromium-browser
+
+# Changing Default User Agent
+sudo rm /usr/share/applications/chromium-browser.desktop
+sudo cp chromium-browser.desktop /usr/share/applications/chromium-browser.desktop
+
+# Cleaning Downloads
+cd $installdir
+rm -rf chrome
+
+# All Done
+echo All Done
+exit 0
